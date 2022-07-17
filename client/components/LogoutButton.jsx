aea4a542047fcb1e6c05f806afb5e2d4ef78f925
Mark Alexander <markalexander@example.com>
2022-07-16 19:10:30 -0700

    Removed unused imports in logoutbutton

--- client/components/LogoutButton.jsx
+++ client/components/LogoutButton.jsx
@@ -1,15 +1,6 @@
 import React from "react";
-import { useState } from 'react';
-import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
 import "../styles.scss";
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
 
 
